8fd0f595534ecd759049c4387b8a62327f8edb6d
George Lyamzin <62714858+GeorgeLMZN@users.noreply.github.com>
2022-12-20 11:31:06 +0400

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
-
 // vue.config.js
 module.exports = {
+	chainWebpack: config => {
+		config.module
+			.rule('vue')
+			.test(/\.vue$/)
+			.use('vue-loader')
+			.loader('vue-loader')
+			.end();
 
+		config.module
+	  		.rule('js')
+	  		.test(/\.js$/)
+	  		.use('babel-loader')
+	  		.loader('babel-loader')
+			.end();
+	}
 }
